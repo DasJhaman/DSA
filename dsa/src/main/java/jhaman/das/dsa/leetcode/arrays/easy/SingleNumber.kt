1727c5243c27e81a150fdf519130178e2707f8e2
jhamandas <jhaman.das@arconsis.com>
2023-10-25 18:43:16 +0200

    Single number problem leetcode.

---
+++ dsa/src/main/java/jhaman/das/dsa/leetcode/arrays/easy/SingleNumber.kt
@@ -0,0 +1,14 @@
+package jhaman.das.dsa.leetcode.arrays.easy
+
+
+fun main() {
+    println(singleNumber(intArrayOf(4, 4, 1)))
+}
+
+fun singleNumber(nums: IntArray): Int {
+    var bits = 0
+    nums.forEach {
+        bits = bits xor it
+    }
+    return bits
+}
